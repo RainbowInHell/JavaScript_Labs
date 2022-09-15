640b84ee64118dd982c0e62cf254868d939e5fd2
RainbowInHell <matveyrfh@mail.ru>
2022-09-15 17:30:08 +0300

    First,Second,Third labs have been added

---
+++ Lab1_Js_Bykovsky/Scenario.js
@@ -0,0 +1,14 @@
+//function Cringe() {
+//    alert("Я сдаю телефон на парах")
+//}
+
+function ThirdTask() {
+    let admin, name = "Джон";
+    admin = name;
+    alert(admin)
+}
+
+function FourthTask() {
+    let planetName = "NeZemlya"
+    let currentUser = null
+}
